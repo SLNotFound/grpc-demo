b74b738c40478449e4fbd8f80c0aeea42b8af30d
sunl <sunlight0915@163.com>
2022-05-29 22:18:08 +0800

    Client-Side & Bidirectional streaming RPC

--- client/stream_client/client.go
+++ client/stream_client/client.go
@@ -32,10 +32,51 @@ func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
 }
 
 func printRecord(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	stream, err := client.Record(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for n := 0; n < 6; n++ {
+		err := stream.Send(r)
+		if err != nil {
+			return err
+		}
+	}
+
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
 	return nil
 }
 
 func printRoute(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	stream, err := client.Route(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for n := 0; n < 6; n++ {
+		err = stream.Send(r)
+		if err != nil {
+			return err
+		}
+
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("resp: pj.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+	}
+
+	stream.CloseSend()
 	return nil
 }
 
--- server/stream_server/server.go
+++ server/stream_server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"google.golang.org/grpc"
 	pb "grpc-demo/proto/proto"
+	"io"
 	"log"
 	"net"
 )
@@ -26,10 +27,45 @@ func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListSe
 }
 
 func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.StreamResponse{Pt: &pb.StreamPoint{Name: "gRPC Stream Server: Record", Value: 1}})
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
 	return nil
 }
 
 func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
+	n := 0
+	for {
+		err := stream.Send(&pb.StreamResponse{
+			Pt: &pb.StreamPoint{
+				Name:  "gRPC Stream Client: Route",
+				Value: int32(n),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		n++
+
+		log.Printf("Stream.Recv pt.name: %s, pt.value: %d", r.Pt.Name, r.Pt.Value)
+	}
+
 	return nil
 }
 
